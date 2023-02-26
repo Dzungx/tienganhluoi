3c6fc1f3379c08bb6f6e248a759b4d77a64dc9fb
Dzungx <34862664+Dzungx@users.noreply.github.com>
2023-02-26 16:31:24 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,15 +2,15 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '0074c5651737421caa583649381e75d6',
+  rootNotionPageId: '8b7cfdd26f224c1da8b7c5fbb80114f0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Anh Villas',
-  domain: 'anhvillas.com',
+  name: 'Tieng Anh Luoi',
+  domain: 'tienganhluoi.com',
   author: 'Dung Nguyen Anh',
 
   // open graph metadata (optional)
